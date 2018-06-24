7a95310575747b7c6791b599e176457178e467e7
Lander Beeuwsaert <lander.beeuwsaert@gmail.com>
2018-06-24 13:44:46 +0200

    remove all

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,12 @@
-import './polyfills.ts';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
-
-import {Observable} from "rxjs/Observable";
-
-
-const debuggerOn = true;
-
-import 'rxjs/add/operator/do';
-
-declare module 'rxjs/Observable' {
-    interface Observable<T> {
-        debug: (...any) => Observable<T>
-    }
-}
-
-Observable.prototype.debug = function(message:string) {
-    return this.do(
-        nextValue => {
-            if (debuggerOn) {
-                console.log(message, nextValue)
-            }
-        },
-        error => {
-            if (debuggerOn) {
-                console.error(message, error)
-            }
-        },
-        () => {
-            if (debuggerOn) {
-                console.error("Observable completed - ", message)
-            }
-        }
-    );
-};
-
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/skip';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/withLatestFrom';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/catch';
-
-import "rxjs/add/observable/interval";
-import "rxjs/add/observable/of";
-
-
-
-
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
-
-
-
-
-
-
-
-
-
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
